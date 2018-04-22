5d8e51c1f8f716166b28d51f191ac6e8d5b1c0e3
André Andrade <ardy.aileron@gmail.com>
2018-04-22 17:37:55 -0300

    just one drink
    no hangover

--- scripts/character.gd
+++ scripts/character.gd
@@ -3,12 +3,15 @@ extends Node2D
 onready var drink = load("res://objects/drink.tscn")
 onready var hand1 = $hand1
 onready var hand2 = $hand2
+var drinking = false
 
 func _ready():
 	print(hand1)
 	print(hand2)
 
 func give_drink():
-	var a = drink.instance()
-	a.set_position(hand1.get_position())
-	add_child(a)
+	if not drinking():
+		var a = drink.instance()
+		a.set_position(hand1.get_position())
+		add_child(a)
+		drinking = true
